0c202092966d16cb3f05d4a9722a4936612d2388
Morales Gonzalez Ricardo (CI/PFM3-LA) <rri2tl@bosch.com>
2022-02-10 10:51:33 -0600

    Temperature Asserts

--- checker.cs
+++ checker.cs
@@ -4,16 +4,18 @@ using System.Diagnostics;
 class Checker
 {
     static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-        }
+        //if(temperature < 0 || temperature > 45) {
+        //    Console.WriteLine("Temperature is out of range!");
+        //    return false;
+        //} else if(soc < 20 || soc > 80) {
+        //    Console.WriteLine("State of Charge is out of range!");
+        //    return false;
+        //} else if(chargeRate > 0.8) {
+        //    Console.WriteLine("Charge Rate is out of range!");
+        //    return false;
+        //}
+
+        return rangeIsOK(0f, 45f, temperature) && rangeIsOK(20f, 80f, soc) && temperatureUnderLimit(0.8f, chargeRate);
         return true;
     }
 
@@ -33,6 +35,37 @@ class Checker
         ExpectTrue(batteryIsOk(25, 70, 0.7f));
         ExpectFalse(batteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
+
+        Debug.Assert(rangeIsOK(20f, 80f, 18.5f) == false, "State of charge under range");
+        Debug.Assert(temperatureUnderLimit(0.9f, 0.81f) == true, "Charge rate under limit");
+       
         return 0;
+    }
+
+
+    static bool rangeIsOK(float MIN, float MAX, float Value)
+    {
+
+        if (Value <= MIN || Value => MAX)
+        {
+
+            return false;
+        }
+
+        return true;
+
+    }
+
+    static bool temperatureUnderLimit(float temperatureLimit, float Value)
+    {
+
+        if (Value => temperatureLimit)
+        {
+
+            return false;
+        }
+
+        return true;
+
     }
 }
