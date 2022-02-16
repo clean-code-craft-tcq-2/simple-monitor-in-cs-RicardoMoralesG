bce5fae4d4e529d21ff6628f4b173eded9606ef9
Morales Gonzalez Ricardo (CI/PFM3-LA) <rri2tl@bosch.com>
2022-02-16 08:39:29 -0600

    checker buld

--- checker.cs
+++ checker.cs
@@ -5,19 +5,10 @@ class Checker
 {
     static bool batteryIsOk(float temperature, float soc, float chargeRate)
     {
-        //if(temperature < 0 || temperature > 45) {
-        //    Console.WriteLine("Temperature is out of range!");
-        //    return false;
-        //} else if(soc < 20 || soc > 80) {
-        //    Console.WriteLine("State of Charge is out of range!");
-        //    return false;
-        //} else if(chargeRate > 0.8) {
-        //    Console.WriteLine("Charge Rate is out of range!");
-        //    return false;
-        //}
+      
 
         return rangeIsOK(0f, 45f, temperature) && rangeIsOK(20f, 80f, soc) && temperatureUnderLimit(0.8f, chargeRate);
-        // return true;
+       
     }
 
     static void ExpectTrue(bool expression)
@@ -42,12 +33,9 @@ class Checker
         ExpectFalse(batteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
 
-        Debug.Assert(temperatureUnderLimit(0.8f, 0.70f) == true);
-        Debug.Assert(temperatureUnderLimit(0.8f, 0.80f) == false);
-
-        Debug.Assert(rangeIsOK(0f, 45f, 0f) == false);
-        Debug.Assert(rangeIsOK(0f, 45f, 46f) == false);
-
+       
+        Debug.Assert(rangeIsOK(0f, 45f, -1.3f) == false, "Temperature under range");
+        Debug.Assert(rangeIsOK(0f, 45f, 45.5f) == false, "Temperature over range");
 
 
 
